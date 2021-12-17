7ee72eee3222c76af1c373d7b65d3bc3253129f6
leandrocgsi <leandrocgsi@gmail.com>
2021-12-16 22:45:26 -0300

    Setup REST Controller PersonController Endpoints Tests

--- src/test/java/br/com/erudio/controller/AuthControllerTest.java
+++ src/test/java/br/com/erudio/controller/AuthControllerTest.java
@@ -3,14 +3,11 @@ package br.com.erudio.controller;
 import static io.restassured.RestAssured.given;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.com.erudio.security.AccountCredentialsVO;
 import br.com.erudio.vo.LoginResponseVO;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AuthControllerTest {
 
